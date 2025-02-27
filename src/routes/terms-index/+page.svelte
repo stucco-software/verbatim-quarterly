5d1c9c391e920af45f9638e3534489b40f399a42
nikolaswise <nikolaswise@gmail.com>
2025-02-27 13:12:43 -0800

    term filtering

--- src/routes/terms-index/+page.svelte
+++ src/routes/terms-index/+page.svelte
@@ -1,13 +1,34 @@
 <script>
   import NarrowHeader from "$lib/components/NarrowHeader.svelte"
   let { data } = $props()
-  let { index } = data
+  let terms = $state(data.index.collects)
+  let find = $state('')
+  $effect(() => {
+    if (find.length > 0) {
+      let term = find.toLowerCase()
+      terms = data.index.collects.filter(n => {
+        return n.html.toLowerCase().includes(term)
+      })
+    } else {
+      terms = data.index.collects
+    }
+  })
+
 </script>
 
 <NarrowHeader />
 
 <h1>Index</h1>
 
-{#each index.collects as term}
+<form>
+  <label>
+    Find:
+    <input type="type" name="find" bind:value={find}>
+  </label>
+
+</form>
+
+
+{#each terms as term}
   {@html term.html}
 {/each}
